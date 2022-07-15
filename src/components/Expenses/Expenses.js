967a47403eff1356dfea0e8a92815ad90a52afd0
Oloche Richard <98244432+Oloche-Richard@users.noreply.github.com>
2022-07-15 22:21:22 +0100

    commit

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -1,5 +1,5 @@
 import {useState} from "react"
-import ExpenseItem from "./ExpenseItem";
+import ExpenseList from "./ExpenseList";
 import Card from "../UI/Card.js"
 import "./Expenses.css";
 import ExpensesFilter from './ExpensesFilter';
@@ -16,25 +16,13 @@ function Expenses(props) {
      return expense.date.getFullYear().toString() === filteredYear
    });
 
-  let expenseContent = <p>No expenses found</p>;
-   if(filteredExpenses.length > 0 ) {
-     expenseContent =  filteredExpenses.map((expense) =>    
-    <ExpenseItem 
-    key={expense.id}
-    title={expense.title}
-    amount={expense.amount}
-    date={expense.date}
-     />
-    )
-   }
-
   return (
     <Card className="expenses">
        <ExpensesFilter 
        selected={filteredYear} 
        onChangeFilter={filterChangeHandler}
        />
-       {expenseContent}
+       <ExpenseList items= {filteredExpenses}/>
   </Card>
   );
 }
